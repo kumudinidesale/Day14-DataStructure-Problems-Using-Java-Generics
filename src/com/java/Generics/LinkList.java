a73eedbb00ed6db68aa4d794d3cd553ed34e9f0c
kumudinidesale <kdesale1211@gmail.com>
2022-02-26 17:23:59 +0530

    added:Ability to delete first

--- src/com/java/Generics/LinkList.java
+++ src/com/java/Generics/LinkList.java
@@ -67,21 +67,29 @@ public class LinkList {
 	 * add middle method
 	 */
 
-	public void addMiddle(Object data) {
+	public void addMiddle(Object data, Object data1) {
 		Node newNode = new Node(data);
 
 		if (head == null)
 			head = newNode;
 		else {
 			Node temp = head;
-			System.out.println("Enter the data after which new data should be added");
-			Object data1 = scanner.nextInt();
 			while (temp.data != data1) {
 				temp = temp.next;
 			}
 			newNode.next = temp.next;
 			temp.next = newNode;
 		}
+	}
+
+	/*
+	 * delete last method
+	 */
+	public void deleteFirst() {
+		if (head == null)
+			System.out.println("No elements present to delete");
+		else
+			head = head.next;
 	}
 
 }
